2220d085683f22e8ead05fd4ca2aa57741432177
Sourav Sarkar <46227372+sourav03561@users.noreply.github.com>
2025-11-15 03:18:29 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://13.61.14.126:8000/',
-        changeOrigin: true
-      }
-    }
-  }
 })
